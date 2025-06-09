8836f34c7202f7567730f8d48b96e95572cba9c1
FreeWifi0802 <104181615+FreeWifi0802@users.noreply.github.com>
2025-06-09 18:53:48 -0400

    Implemented Working Timer With React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const _second = 1000;
+  const _minute = _second * 60;
+  const _hour   = _minute * 60;
+  const _day    = _hour * 24;
 
-  return (
+function App() {
+  const releaseDate = new Date("6/9/2025 8:30AM");
+  const [timeLeft, setTimeLeft] = useState(releaseDate - new Date());
+
+  useEffect(() => {
+      let timer = setInterval(() => {
+      updateTimer(releaseDate)
+      }, _second);
+
+     return () => {
+      clearInterval(timer);
+     }
+  }, [releaseDate]);
+
+  function updateTimer(goalTime){
+    setTimeLeft(goalTime - new Date());
+  }
+
+   if (timeLeft <= 0){
+    return (
+      <>
+      <h1>The Switch has been released!</h1>
+      </>
+    );
+  } else {
+    const daysLeft = String(Math.floor(timeLeft / _day)).padStart(2, '0');
+            const hoursLeft = String(Math.floor((timeLeft % _day) / _hour)).padStart(2, '0');
+            const minutesLeft = String(Math.floor((timeLeft % _hour) / _minute)).padStart(2, '0');
+            const secondsLeft = String(Math.floor((timeLeft % _minute) / _second)).padStart(2, '0');
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Time Left: Days: {daysLeft} Hours: {hoursLeft} Minutes: {minutesLeft} Seconds: {secondsLeft}s</h1>
     </>
-  )
+    )
+  }
+
+  
 }
 
 export default App
